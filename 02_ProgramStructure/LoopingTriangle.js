7444ba0d8af5a452ccd80bf36352d27593ccfaca
Mikael Rasimus <mikael.rasimus@gmail.com>
2016-08-17 20:56:16 +0300

    added the problem descriptions to the files as comments

--- 02_ProgramStructure/LoopingTriangle.js
+++ 02_ProgramStructure/LoopingTriangle.js
@@ -1,3 +1,21 @@
+/*
+Looping a triangle
+Write a loop that makes seven calls to console.log to output the following
+triangle:
+#
+##
+###
+####
+#####
+# # # # # #
+# # # # # # #
+It may be useful to know that you can find the length of a string by
+writing .length after it.
+var abc = " abc ";
+console . log (abc.length) ;
+// -> 3
+*/
+
 for (var i = "#"; i.length < 8; i+= "#") {
 
 	console.log(i);
